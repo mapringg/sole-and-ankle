bffe0cd74f5c28ce6ae416cef1b48b774f0efa0e
Sorawit Kongnurat <work@sorawitk.com>
2022-12-09 02:17:37 +0700

    feat: finish exercise 1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -26,10 +26,18 @@ const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
+
+  display: flex;
+  gap: 24px;
+  padding: 12px 32px;
 `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
+
+  ${Wrapper} & {
+    margin-right: auto;
+  }
 `;
 
 const HelpLink = styled.a`
